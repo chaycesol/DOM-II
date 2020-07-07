1ad59a15a5395db6cc7bfb82edc4bd9621447739
Chayce SOlchaga <chayce.solchaga@gmail.com>
2020-07-07 13:47:16 -0700

    Commenting out tasks in JS file

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
 // Your code goes here
+
+//Create 10 unique Event Listners
+// mouseover
+// keydown
+// wheel
+// drag / drop
+// load
+// focus
+// resize
+// scroll
+// select
+// dblclick
+
+
+// Nest two similar events somewhere in the site and prevent the event propagation properly
+
+//Stop the navigation items from refreshing the page by using `preventDefault()
+document.querySelector('a').addEventListener('click', function(evt){
+    evt.preventDefault()
+})
